0d2af07ad5b1c91a63808787b86c8f0327aac086
Syanmil <syanmil@yahoo.com>
2017-01-11 10:02:28 +0700

    skeleton created

--- todo.js
+++ todo.js
@@ -1,1 +1,37 @@
+"use strict"
 
+class Task{
+  constructor(){
+    this.id = id;
+    this.name = name;
+    this.completed = "[ ]"
+    this.created = new Date()
+  }
+}
+
+class ToDo{
+  constructor(){
+
+  }
+  static help(){
+
+  }
+  static list(){
+
+  }
+  static add(content){
+
+  }
+  static task(id){
+
+  }
+  static delete(id){
+
+  }
+  static complete(id){
+
+  }
+  static uncomplete(id){
+
+  }
+}
